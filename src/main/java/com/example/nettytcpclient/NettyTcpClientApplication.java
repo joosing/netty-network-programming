4be2f81fc2f9fe87007fb2ff1d79f94b3248546d
jsing <joosing711@gmail.com>
2021-09-05 09:27:56 +0900

    init.

---
+++ src/main/java/com/example/nettytcpclient/NettyTcpClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.nettytcpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyTcpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyTcpClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nettytcpclient/NettyTcpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nettytcpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyTcpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
